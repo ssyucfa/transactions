437a5b64efecd9515b8e8a81dcb4df85f51fa537
Ivan Patin <payk1230@bk.ru>
2023-03-08 00:08:43 +0300

    edits

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -9,6 +9,8 @@ def create_transaction(user_id: int, transaction_amount: int, transaction_type: 
     with SessionLocal() as db:
         current_user = user.get(db, user_id)
 
+        # In the time interval between the execution of the task,
+        # the money can already be withdrawn, so here is this check
         if transaction_type == TransactionType.WITHDRAW.value and transaction_amount > current_user.balance:
             # TODO: Добавить оповещение о том, что недостаточно денег
             print("Мало денег на балансе")
